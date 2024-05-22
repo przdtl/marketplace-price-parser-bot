74f0a189ce84a5a0baaf65909d76f304387a5064
Sinyushkin Timofey <y.timko56@gmail.com>
2024-05-22 22:18:53 +0700

    create handlers for documnet and all others

--- src/routers/commands.py
+++ src/routers/commands.py
@@ -1,6 +1,12 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
+from src.services.excel_services import (
+    get_document_from_message_and_download_if_extention_is_excel,
+    send_document_if_exists,
+    get_excel_filename_by_chat_id
+)
 
 
 router = Router()
@@ -11,6 +17,19 @@ async def command_start_handler(message: Message) -> None:
     await message.answer('Привет **{}**'.format(message.from_user.full_name))
 
 
+@router.message(F.document)
+async def documnets_messages_handler(message: Message) -> None:
+
+    path_to_excel_document = await get_document_from_message_and_download_if_extention_is_excel(
+        message=message,
+        destination_file_name=get_excel_filename_by_chat_id(message)
+    )
+    await message.answer('Файл успешно сохранён!' if path_to_excel_document else 'Ошибка файла!')
+
+
 @router.message()
-async def other_messages_handler(message: Message) -> None:
-    pass
+async def all_other_messages_handler(message: Message) -> None:
+    excel_file_path = get_excel_filename_by_chat_id(message)
+
+    if not await send_document_if_exists(message, excel_file_path):
+        await message.answer('Вы ещё не сохраняли документ!')
