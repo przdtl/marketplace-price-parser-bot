02eb9157aa424daceda22629ebda8a7de38009c1
Sinyushkin Timofey <y.timko56@gmail.com>
2024-06-10 20:10:24 +0700

    create main_menu.py router

---
+++ src/routers/main_menu.py
@@ -0,0 +1,34 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+from src.enums import NonStateEnums
+from src.states import (ProductsStatesGroup, SettingsStatesGroup)
+from src.keyboards import (get_products_keyboard,
+                           load_products_keyboard, settings_keyboard)
+
+router = Router()
+
+
+@router.message(
+    F.text == NonStateEnums.LOAD_PRODUCTS,
+)
+async def load_products_message_handler(message: Message, state: FSMContext) -> None:
+    await message.answer('Вы перешли в меню загрузки информации о товарах', reply_markup=load_products_keyboard())
+    await state.set_state(ProductsStatesGroup.add_new_articuls)
+
+
+@router.message(
+    F.text == NonStateEnums.GET_PRODUCTS,
+)
+async def get_products_message_handler(message: Message, state: FSMContext) -> None:
+    await message.answer('Вы перешли в меню получения информации о товарах', reply_markup=get_products_keyboard())
+    await state.set_state(ProductsStatesGroup.get_products_list)
+
+
+@router.message(
+    F.text == NonStateEnums.SETTINGS,
+)
+async def settings_message_handler(message: Message, state: FSMContext) -> None:
+    await message.answer('Вы перешли в настройки', reply_markup=settings_keyboard())
+    await state.set_state(SettingsStatesGroup.base_settings)
