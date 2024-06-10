53f8005f2c436cd640b6446cff09ade34af99654
Sinyushkin Timofey <y.timko56@gmail.com>
2024-06-10 20:09:44 +0700

    update commands.py

--- src/routers/commands.py
+++ src/routers/commands.py
@@ -7,6 +7,7 @@ from src.services.excel_services import (
     send_document_if_exists,
     get_excel_filename_by_chat_id
 )
+from src.keyboards import main_manu_keyboard
 
 
 router = Router()
@@ -14,22 +15,19 @@ router = Router()
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer('Привет **{}**'.format(message.from_user.full_name))
+    await message.answer('Привет **{}**'.format(message.from_user.full_name), reply_markup=main_manu_keyboard())
 
 
-@router.message(F.document)
-async def documnets_messages_handler(message: Message) -> None:
+# @router.message(F.document)
+# async def documnets_messages_handler(message: Message) -> None:
 
-    path_to_excel_document = await get_document_from_message_and_download_if_extention_is_excel(
-        message=message,
-        destination_file_name=get_excel_filename_by_chat_id(message)
-    )
-    await message.answer('Файл успешно сохранён!' if path_to_excel_document else 'Ошибка файла!')
+#     path_to_excel_document = await get_document_from_message_and_download_if_extention_is_excel(
+#         message=message,
+#         destination_file_name=get_excel_filename_by_chat_id(message)
+#     )
+#     await message.answer('Файл успешно сохранён!' if path_to_excel_document else 'Ошибка файла!')
 
 
 @router.message()
 async def all_other_messages_handler(message: Message) -> None:
-    excel_file_path = get_excel_filename_by_chat_id(message)
-
-    if not await send_document_if_exists(message, excel_file_path):
-        await message.answer('Вы ещё не сохраняли документ!')
+    await message.answer('Я не знаю такой команды!')
