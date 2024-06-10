f47c5f2e8f5d01ddb6e44ea78c67f10eb227988d
Sinyushkin Timofey <y.timko56@gmail.com>
2024-06-10 20:07:25 +0700

    create enum.py

---
+++ src/enums.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class CommonButtonsNames(str, Enum):
+    BACK = 'Назад'
+
+
+class NonStateEnums(str, Enum):
+    LOAD_PRODUCTS = 'Загрузить'
+    GET_PRODUCTS = 'Получить'
+    SETTINGS = 'Настройки'
+
+
+class SettingsStateEnum(str, Enum):
+    SET_SCRAPPING_TIME = 'Установить время сканирования'
+
+
+class GetProductsStateEnums(str, Enum):
+    GET_PRODUCTS_IN_TEXT = 'Получить инормацию сообщением'
+    GET_PRODUCTS_IN_EXCEL = 'Получить информациею в Excel'
+
+
+class LoadProductsStateEnums(str, Enum):
+    pass
