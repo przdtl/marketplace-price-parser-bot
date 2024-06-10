34bbb5925dfd26dcd76587c593b2ee748be15aa4
Sinyushkin Timofey <y.timko56@gmail.com>
2024-06-10 20:11:30 +0700

    include routers in main.py

--- src/main.py
+++ src/main.py
@@ -4,10 +4,18 @@ import logging
 
 from src.settings.loader import dispatcher, bot
 from src.routers.commands import router as commands_router
+from src.routers.main_menu import router as main_menu_router
+from src.routers.products import router as products_router
+from src.routers.settings import router as settings_router
 
 
 async def main() -> None:
-    dispatcher.include_router(commands_router)
+    dispatcher.include_routers(
+        products_router,
+        main_menu_router,
+        settings_router,
+        commands_router
+    )
     await dispatcher.start_polling(bot)
 
 
