8a0c435bbefc99298999aa779c4baf1a07f957bb
Sinyushkin Timofey <y.timko56@gmail.com>
2024-06-11 12:32:50 +0700

    structuring variables in config.py

--- src/config.py
+++ src/config.py
@@ -1,19 +1,28 @@
-from pathlib import Path
 from pydantic_settings import (BaseSettings, SettingsConfigDict)
 
 
 class Settings(BaseSettings):
 
     BOT_TOKEN: str
-    PATH_TO_EXCEL_FOLDER: str = str(
-        Path(__file__).resolve().parent.parent.parent) + "\\media\\excel\\"
+
+    # MongoDB variables
+    MONGODB_HOST: str
+    MONGODB_PORT: str
+    MONGODB_USER: str
+    MONGODB_PASS: str
 
     ALLOWED_EXCEL_EXTENTIONS: list[str] = ['.xls', '.xlsx']
 
-    #
+    # URL of site's products
     OZON_PRODUCT_URL: str = 'https://www.ozon.ru/product'
+
+    # Prise parser variables 
     PRICE_PARSER_SERVICE_URL: str = 'http://127.0.0.1:8000'
     OZON_PRICE_PARSER_SERVICE_URL: str = PRICE_PARSER_SERVICE_URL + '/ozon'
-    # TODO: add wildberries support
+    WILDBERRIES_PRICE_PARSER_SERVICE_URL: str = PRICE_PARSER_SERVICE_URL + '/wildberries'
+
+    @property
+    def mongodb_dsn(self) -> str:
+        return 'mongodb://{}:{}@{}:{}'.format(self.MONGODB_USER, self.MONGODB_PASS, self.MONGODB_HOST, self.MONGODB_PORT)
 
     model_config = SettingsConfigDict(env_file='.env')
