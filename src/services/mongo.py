0be4258e41f87d8bc348725c13c819fb66228f7a
Sinyushkin Timofey <y.timko56@gmail.com>
2024-06-15 22:13:36 +0700

    update services mongo and tg add service for deleting all products for user in db create services for reading documents in tg chat

--- src/services/mongo.py
+++ src/services/mongo.py
@@ -13,6 +13,9 @@ async def add_list_of_products_in_document(chat_id: int, marketplace_name: str, 
     articles_not_present = await get_not_existed_articuls(
         chat_id, marketplace_name, articuls)
 
+    if not articles_not_present:
+        return
+
     for articul in articles_not_present:
         document_record.append({
             'chat_id': chat_id,
@@ -23,6 +26,14 @@ async def add_list_of_products_in_document(chat_id: int, marketplace_name: str, 
 
     await collection.insert_many(document_record)
 
+
+async def delete_all_products_from_user_in_specific_marketplace(chat_id: int, marketplace_name: str) -> None:
+    '''Удаляет все записи о товарах у пользователя в конкретном маркетплейсе'''
+    user_filter = {
+        'chat_id': chat_id,
+        'marketplace_name': marketplace_name,
+    }
+    await collection.delete_one(user_filter)
 
 # async def add_new_price_to_product(chat_id: int, marketplace_name: str, articul: int, new_price: float) -> None:
 #     '''Добавляет цену в список конкретному товару'''
@@ -95,19 +106,3 @@ async def get_product_prices_from_user_from_specific_marketplace_by_articul(chat
     prices = await collection.find_one(product_filter)
 
     return prices['prices'] if prices else []
-
-
-async def main():
-    await collection.drop()
-    await add_list_of_products_in_document(12345, MarketplaceNameEnum.OZON, [12345, 1234, 123, 12, 1])
-    # await add_new_price_to_product(12345, MarketplaceNameEnum.OZON, 1234, 500.0)
-    # await add_new_price_to_product(12345, MarketplaceNameEnum.OZON, 1234, 34.5)
-    # await add_new_price_to_product(12345, MarketplaceNameEnum.OZON, 12, 45.0)
-    # await add_new_price_to_product(12345, MarketplaceNameEnum.OZON, 123, 33.0)
-    # await add_new_price_to_product(12345, MarketplaceNameEnum.OZON, 1, 50.0)
-
-    print(await get_all_users_articuls_and_its_prices_of_specific_marketplace(12345, MarketplaceNameEnum.OZON))
-    print(await get_product_prices_from_user_from_specific_marketplace_by_articul(12345, MarketplaceNameEnum.OZON, 1234))
-
-if __name__ == '__main__':
-    asyncio.run(main())
--- src/services/tg_document.py
+++ src/services/tg_document.py
@@ -1,11 +1,16 @@
 import aiohttp
 import hashlib
 
+from typing import Any
+
 from aiogram.types import Message, File
 from aiogram.types.input_file import BufferedInputFile
 
 from src.config import Settings
 from src.loader import bot
+from src.enums import (
+    ExtendOrReplaceArticulsEnum
+)
 from src.services.pandas_service import (
     read_first_column_dataframe_from_excel,
     read_first_column_dataframe_from_csv,
@@ -15,7 +20,8 @@ from src.services.pandas_service import (
     get_csv_bytestr_from_dataframe
 )
 from src.services.mongo import (
-    add_list_of_products_in_document
+    add_list_of_products_in_document,
+    delete_all_products_from_user_in_specific_marketplace
 )
 
 
@@ -51,6 +57,7 @@ async def read_first_column_from_sended_excel_document(message: Message) -> list
 
 
 async def read_first_column_from_sended_csv_document(message: Message) -> list[int]:
+    '''Получает из сообщения объект документа и конвертирует данный CSV документ в объект ``pandas`` ``DataFrame``'''
     document = await get_document_from_message(message)
     path_to_file_in_tg_server = 'https://api.telegram.org/file/bot{}/{}'.format(
         Settings().BOT_TOKEN, document.file_path
@@ -83,40 +90,31 @@ async def send_csv_with_products_info(message: Message, marketplace_name: str) -
     )
 
 
-async def write_articuls_from_excel_in_db(message: Message, marketplace_name: str) -> None:
+async def get_list_of_articuls_from_message_document(message: Message) -> list[int]:
+    '''Возвращает список артикулов, считанных из отправленного документа. Валидные типы: Excel и CSV'''
     try:
-        articuls_list = await read_first_column_from_sended_excel_document(message)
-    except ValueError as e:
-        if "Excel file format cannot be determined" in str(e):
-            await message.answer('Вы передали файл с неверным расширением!')
-    else:
-        await add_list_of_products_in_document(
+        return await read_first_column_from_sended_excel_document(message)
+    except ValueError:
+        pass
+    try:
+        return await read_first_column_from_sended_csv_document(message)
+    except ValueError:
+        raise ValueError('The document has an invalid extension')
+
+
+async def write_articuls_in_db(message: Message, answer_option: str, state_data: dict[str, Any]) -> None:
+    '''Записывает артикулы товаров в БД. Присутствует выбор добавления артикулов к уже существующим или полная замена старых на новые'''
+    marketplace_name = state_data.get('marketplace_name')
+    articuls_list = state_data.get('articuls')
+
+    if answer_option == ExtendOrReplaceArticulsEnum.REPLACE.value:
+        await delete_all_products_from_user_in_specific_marketplace(
             chat_id=message.chat.id,
             marketplace_name=marketplace_name,
-            articuls=articuls_list
         )
 
-
-async def write_articuls_from_csv_in_db(message: Message, marketplace_name: str) -> None:
-    articuls_list = await read_first_column_from_sended_csv_document(message)
     await add_list_of_products_in_document(
         chat_id=message.chat.id,
         marketplace_name=marketplace_name,
         articuls=articuls_list
     )
-
-# async def get_product_prices_by_articuls(*articuls) -> list[float]:
-#     '''Отправляет запрос на сервис парсинга с данными о артикулах и возвращает список цен на эти товары'''
-#     url = Settings().OZON_PRICE_PARSER_SERVICE_URL
-#     request_body = {
-#         'articuls': articuls
-#     }
-
-#     prices = []
-
-#     async with aiohttp.ClientSession() as session:
-#         response = await session.post(url, json=request_body)
-#         json_answer = await response.json()
-#         prices = json_answer['prices']
-
-#     return prices
