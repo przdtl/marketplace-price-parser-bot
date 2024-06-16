52b9ca2586e02be9d6a329a0cbb6a57e5ac4fb1b
Sinyushkin Timofey <y.timko56@gmail.com>
2024-06-16 12:18:47 +0700

    update reading from csv service

--- src/services/pandas_service.py
+++ src/services/pandas_service.py
@@ -1,11 +1,8 @@
 import io
+
 import pandas as pd
 
-from typing import Union
-
 from src.services.mongo import get_all_users_articuls_and_its_prices_of_specific_marketplace
-
-pd.options.mode.chained_assignment = None
 
 
 def read_first_column_dataframe_from_excel(path_to_document: str) -> pd.DataFrame:
@@ -18,7 +15,7 @@ def read_first_column_dataframe_from_excel(path_to_document: str) -> pd.DataFram
 
 def read_first_column_dataframe_from_csv(path_to_document: str) -> pd.DataFrame:
     '''Возвращает DataFrame по первому столбцу, считанному из CSV файла по переданному пути'''
-    df = pd.read_csv(io=path_to_document, header=None,
+    df = pd.read_csv(filepath_or_buffer=path_to_document, header=None,
                      index_col=None, usecols=[0])
 
     return df
@@ -62,32 +59,3 @@ async def get_table_of_articuls_and_its_prices(chat_id: int, marketplace_name: s
         table.append(row)
 
     return pd.DataFrame(table)
-
-
-# def parse_valid_ozon_articuls_from_str(message: str) -> list[int]:
-#     splitted_articuls = message.split(',')
-#     articuls = []
-#     for articul in splitted_articuls:
-#         try:
-#             articul = int(articul.strip())
-
-#         except ValueError:
-#             pass
-
-#         else:
-#             articuls.append(articul)
-
-#     return articuls
-
-# def delete_existing_articuls(existing_articuls: list[int], new_articuls: list[int]) -> list[int]:
-#     articuls = [x for x in new_articuls if x not in existing_articuls]
-
-#     return articuls
-
-# def get_links_to_products_by_articuls(*articuls: int) -> list[str]:
-#     url_to_products = Settings().OZON_PRODUCT_URL
-#     links = []
-#     for articul in articuls:
-#         links.append(url_to_products + str(articul))
-
-#     return links
